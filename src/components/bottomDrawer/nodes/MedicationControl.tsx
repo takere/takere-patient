1c0faeb60dcc52da7c78fe31a5bf1f0483661afb
William Niemiec <williamniemiec@hotmail.com>
2022-08-28 13:31:54 -0300

    Medication control - bottom drawer

--- src/components/bottomDrawer/nodes/MedicationControl.tsx
+++ src/components/bottomDrawer/nodes/MedicationControl.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Button, Heading, Text, Input, useToast} from 'native-base';
+import {Button, Heading, Text, Input, useToast, Divider, HStack, VStack, Tooltip } from 'native-base';
 import IHandler from '../../../models/IHandler';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import {Requests} from '../../../services/axios/remoteRequests';
 import HandleSubmit from '../HandleSubmit';
 
@@ -14,34 +15,57 @@ const MedicationControlHandler = ({data, onUpdateData}: IHandler) => {
     onUpdateData();
   };
 
+  const beginDate = formatDateHour(new Date(data.node.results.beginDate))
+  let endDate;
+
+  if (data.node.results.undefinedEnd) {
+    endDate = 'Undefined';
+  }
+  else {
+    endDate = formatDateHour(new Date(data.node.results.endDate));
+  }
+
   return (
     <>
       <Heading size="xl" ml={3} mt={1} color="muted.800">
         {data.node.results.name ?? 'Name'}
       </Heading>
-      <Text ml={3} mt={1}>
-        {data.node.results.description ?? 'Desc'}
-      </Text>
-      <Text ml={3} mt={1}>
-        {data.node.results.why}
-      </Text>
-      <Text ml={3} mt={1}>
-        {data.node.results.notes}
-      </Text>
-      <Text ml={3} mt={1}>
-        {data.node.results.dosage}
-      </Text>
-      <Text ml={3} mt={1}>
-        {data.node.results.beginDate}
-      </Text>
-      <Text ml={3} mt={1}>
-        {data.node.results.endDate}
-      </Text>
-      <Text ml={3} mt={1}>
-        {data.node.results.undefinedEnd}
-      </Text>
+      <VStack space={3} divider={<Divider />} w="100%" p='10'>
+        <HStack justifyContent="space-between">
+          <Text>{data.node.results.description ?? 'Desc'}</Text>
+          <Icon name="notes" size={30} />
+        </HStack>
+        <HStack justifyContent="space-between">
+          <Text>{data.node.results.why}</Text>
+          <Icon name="help" size={30} />
+        </HStack>
+        <HStack justifyContent="space-between">
+          <Text>{data.node.results.notes}</Text>
+          <Icon name="comment" size={30} />
+        </HStack>
+        <HStack justifyContent="space-between">
+          <Text>{data.node.results.dosage}</Text>
+          <Icon name="medical-services" size={30} />
+        </HStack>
+        <HStack justifyContent="space-between">
+          <Text>{beginDate}</Text>
+          <Icon name="play-arrow" size={30} />
+        </HStack>
+        <HStack justifyContent="space-between">
+          <Text>{endDate}</Text>
+          <Icon name="stop" size={30} />
+        </HStack>
+      </VStack>
+      <HandleSubmit onClick={handleSub} />
     </>
   );
 };
 
 export default MedicationControlHandler;
+
+function formatDateHour(date: Date): string {
+  const formattedDate = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
+  const formattedHour = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  
+  return (formattedDate + ' ' + formattedHour);
+}
