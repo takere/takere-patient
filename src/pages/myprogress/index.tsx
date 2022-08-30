14e5ba9e675af08993f2acee5905a4a1179a33dd
William Niemiec <williamniemiec@hotmail.com>
2022-08-30 16:23:11 -0300

    My progress

--- src/pages/myprogress/index.tsx
+++ src/pages/myprogress/index.tsx
@@ -4,7 +4,7 @@ import {
   Center,
   Heading,
   HStack,
-  Pressable,
+  Progress,
   ScrollView,
   Text,
   useToast,
@@ -14,6 +14,8 @@ import {useEffect, useRef, useState} from 'react';
 import {SafeAreaView} from 'react-native';
 import {useUser} from '../../context/user';
 import {Dimensions} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import colors from '../../resources/colors';
 
 export function MyProgressScreen({navigation}: {navigation: any}) {
   const toast = useToast();
@@ -25,9 +27,43 @@ export function MyProgressScreen({navigation}: {navigation: any}) {
     <>
       <SafeAreaView style={{flex: 1}}>
         <ScrollView marginX={3}>
-          
+          <VStack space={2} mt={4}>
+            <ProgressCard
+              bgColor={'#111'}
+              name={'QUIZ'}
+              completed={1}
+              remaining={3}
+            />
+            <ProgressCard
+              bgColor={'#111'}
+              name={'QUIZ'}
+              completed={1}
+              remaining={3}
+            />
+          </VStack>
         </ScrollView>
       </SafeAreaView>
     </>
   );
 }
+
+const ProgressCard = ({ name, completed, remaining, bgColor }: any) => (
+  <HStack p="5" rounded="8" bgColor={bgColor} justifyContent='space-between'>
+    <HStack justifyContent='center'>
+      <Box justifyContent='center' alignItems='center'>
+        <Icon name='help' size={30} color="#fff" />
+      </Box>
+      <Text fontSize={18} color="cyan.50" fontWeight="medium" ml={5}>
+        { name }
+      </Text>
+      <Text fontSize={18} color="cyan.50" fontWeight="medium" ml={5}>
+        ({completed}/{remaining})
+      </Text>
+    </HStack>
+    <Center w="60%">
+      <Box w="90%" maxW="140">
+        <Progress size="md" bg='coolGray.300' _filledTrack={{ bg: colors.success }} value={(completed/remaining) * 100} />
+      </Box>
+    </Center>
+  </HStack>
+);
